51f7d9cd9d66ac46a6393da83d2fb5406231fdab
Daniel Frydman <danielfrydman@id.uff.br>
2023-02-28 18:34:55 -0300

    new route for new controller method

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: 'dashboard#index'
 
   devise_for :users, controllers: { registrations: 'users/registrations' }
+
+  get '/pokemons_purchase/index', to: 'pokemons_purchase#index'
 end
