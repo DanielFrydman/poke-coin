7c8a7900e2172d7acaab5505e3dc19b4e836f1a3
Daniel Frydman <danielfrydman@id.uff.br>
2023-02-28 16:48:54 -0300

    added avatar for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
+  DEFAULT_AVATAR = 'charmander-pokemon.png'
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :lockable, :trackable
+
+  def avatar
+    read_attribute('avatar') || DEFAULT_AVATAR
+  end
 end
