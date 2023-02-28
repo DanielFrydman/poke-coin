5185cdd2b6fd2e42cb07c04ec8d7dd8f6faf1669
Daniel Frydman <danielfrydman@id.uff.br>
2023-02-28 19:02:49 -0300

    new consts and methods into transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,18 @@
 class Transaction < ApplicationRecord
+  BOUGHT_OPERATION = 'bought'.freeze
+  SOLD_OPERATION = 'sold'.freeze
+
   belongs_to :wallet
+
+  def bought?
+    !sold?
+  end
+
+  def sold?
+    sold
+  end
+
+  def pokemon_name_capitalized
+    pokemon_name.capitalize
+  end
 end
