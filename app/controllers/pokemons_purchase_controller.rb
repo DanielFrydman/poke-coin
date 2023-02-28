c485c6bc507ab52933ffdf50f6afb168d13c4c9b
Daniel Frydman <danielfrydman@id.uff.br>
2023-02-28 18:34:32 -0300

    new controller to show all pokemons to buy

---
+++ app/controllers/pokemons_purchase_controller.rb
@@ -0,0 +1,11 @@
+class PokemonsPurchaseController < ApplicationController
+  def index
+    @pokemons = Pokemon::GetAllApi.build.execute
+    @bitcoin_price = Cryptocurrency::GetBitcoinApi.build.price
+  rescue PokemonCommunicationError,
+         CriptocurrencyCommunicationError => e
+    redirect_to root_path, alert: e.message
+  rescue StandardError
+    redirect_to root_path, alert: 'Something went wrong, please try again later.'
+  end
+end
