7671766902d2b08ed48ff05518c6d3d854ed1496
Daniel Frydman <danielfrydman@id.uff.br>
2023-02-28 18:58:02 -0300

    new method in pokemons purchase controller to buy pokemons

--- app/controllers/pokemons_purchase_controller.rb
+++ app/controllers/pokemons_purchase_controller.rb
@@ -6,6 +6,29 @@ class PokemonsPurchaseController < ApplicationController
          CryptocurrencyCommunicationError => e
     redirect_to root_path, alert: e.message
   rescue StandardError
+    redirect_to_root_path_with_error_message
+  end
+
+  def buy
+    service = CreateBuyTransaction.build(chosen_pokemon_url)
+    transaction = service.execute(current_user)
+
+    redirect_to pokemons_purchase_index_path, notice: "#{transaction.pokemon_name_capitalized} bought successfully!"
+  rescue WalletWithoutEnoughFunds,
+         PokemonCommunicationError,
+         CryptocurrencyCommunicationError => e
+    redirect_to pokemons_purchase_index_path, alert: e.message
+  rescue StandardError
+    redirect_to_root_path_with_error_message
+  end
+
+  private
+
+  def chosen_pokemon_url
+    params.require('url')
+  end
+
+  def redirect_to_root_path_with_error_message
     redirect_to root_path, alert: 'Something went wrong, please try again later.'
   end
 end
