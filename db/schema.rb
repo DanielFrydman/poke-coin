ce21b894eb80c824df5deea6710ec4fd80bed38c
Daniel Frydman <danielfrydman@id.uff.br>
2023-02-28 18:19:05 -0300

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_28_211509) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_28_211802) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer "pokemon_id", null: false
+    t.string "pokemon_name", null: false
+    t.integer "pokemon_base_experience", null: false
+    t.float "usd_amount", null: false
+    t.float "btc_amount", null: false
+    t.boolean "sold", default: false, null: false
+    t.string "operation", null: false
+    t.bigint "wallet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["wallet_id"], name: "index_transactions_on_wallet_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
