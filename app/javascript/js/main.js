3e503bc7058bb12a45f43f913a33a766fa031cc6
Daniel Frydman <danielfrydman@id.uff.br>
2023-02-28 16:54:41 -0300

    added sign-out sweetalert

--- app/javascript/js/main.js
+++ app/javascript/js/main.js
@@ -3,6 +3,24 @@ import * as sweetalert2 from "sweetalert2"
 
 window.addEventListener(('turbo:load'), () => {
   document.addEventListener('submit', (event) => {
+    if (event.target && event.target.className === 'sign-out') {
+      event.preventDefault()
+      Swal.fire({
+        title: 'Are you sure?',
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes'
+      })
+        .then((result) => {
+          if (result.isConfirmed) {
+            document.querySelector('.sign-out').submit()
+          }
+        })
+        .catch(event.preventDefault())
+    }
+
     if (event.target && event.target.className === 'delete') {
       event.preventDefault()
       Swal.fire({
