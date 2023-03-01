4ee190fb437f6146c42a59c779dac8084bf3cad9
Daniel Frydman <danielfrydman@id.uff.br>
2023-03-01 18:22:13 -0300

    new sell method into pokemons sale controller

--- app/controllers/pokemons_sale_controller.rb
+++ app/controllers/pokemons_sale_controller.rb
@@ -1,10 +1,39 @@
 class PokemonsSaleController < ApplicationController
   def index
     @transactions = current_user.to_sell_transactions
-    @bitcoin_price = Cryptocurrency::GetBitcoinApi.build.price
+    @bitcoin_price = 1
   rescue CryptocurrencyCommunicationError => e
     redirect_to root_path, alert: e.message
   rescue StandardError
+    redirect_to_root_path_with_error_message
+  end
+
+  def sell
+    service = CreateSellTransaction.build
+    transaction = service.execute(
+      user: current_user,
+      bitcoin_price: bitcoin_price,
+      transaction_id: transaction_id
+    )
+
+    redirect_to pokemons_sale_index_path, notice: "#{transaction.pokemon_name_capitalized} sold successfully!"
+  rescue ActiveRecord::RecordNotFound
+    redirect_to pokemons_sale_index_path, alert: 'Selected transaction not found.'
+  rescue StandardError
+    redirect_to_root_path_with_error_message
+  end
+
+  private
+
+  def transaction_id
+    params.require(:transaction_id)
+  end
+
+  def bitcoin_price
+    params.require(:bitcoin_price).to_f
+  end
+
+  def redirect_to_root_path_with_error_message
     redirect_to root_path, alert: 'Something went wrong, please try again later.'
   end
 end
