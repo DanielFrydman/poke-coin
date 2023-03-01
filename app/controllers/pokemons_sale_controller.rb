8c9da1f38c0709298cee94f3ba63f0c37261a6a8
Daniel Frydman <danielfrydman@id.uff.br>
2023-03-01 18:56:28 -0300

    sort used into pokemons sale controller

--- app/controllers/pokemons_sale_controller.rb
+++ app/controllers/pokemons_sale_controller.rb
@@ -1,6 +1,6 @@
 class PokemonsSaleController < ApplicationController
   def index
-    @transactions = current_user.to_sell_transactions
+    @transactions = current_user.to_sell_transactions.sort
     @bitcoin_price = Cryptocurrency::GetBitcoinApi.build.price
   rescue CryptocurrencyCommunicationError => e
     redirect_to root_path, alert: e.message
