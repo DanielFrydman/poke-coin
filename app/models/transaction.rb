d349a243db7eaa0175394f92c39591e45f96fc23
Daniel Frydman <danielfrydman@id.uff.br>
2023-03-01 18:56:37 -0300

    new methods into transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -15,4 +15,18 @@ class Transaction < ApplicationRecord
   def pokemon_name_capitalized
     pokemon_name.capitalize
   end
+
+  def operation_capitalized
+    operation.capitalize
+  end
+
+  def sold_operation?
+    operation == SOLD_OPERATION
+  end
+
+  def sold_translation
+    return '-' if sold_operation?
+
+    sold? ? 'Yes' : 'No'
+  end
 end
