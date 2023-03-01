b2166d77fb7c525e20ccfbf9542f6294a72c2f69
Daniel Frydman <danielfrydman@id.uff.br>
2023-03-01 14:21:53 -0300

    new route for new controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
 
   get '/pokemons_purchase/index', to: 'pokemons_purchase#index'
   post '/pokemons_purchase/buy', to: 'pokemons_purchase#buy'
+
+  get '/pokemons_sale/index', to: 'pokemons_sale#index'
 end
