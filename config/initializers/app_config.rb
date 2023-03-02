f56b5be338ba10a28aa3df6790af6fa6e2a53548
Daniel Frydman <danielfrydman@id.uff.br>
2023-03-02 20:56:41 -0300

    config added to render get app.yml from root

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,4 +1,8 @@
-CONFIG = YAML.load_file(Rails.root.join('config/app.yml'))[Rails.env]
+CONFIG = if Rails.env.production?
+           YAML.load_file(Rails.root.join('app.yml'))[Rails.env]
+         else
+           YAML.load_file(Rails.root.join('config/app.yml'))[Rails.env]
+         end
 
 module APP_CONFIG
   COIN_API_KEY = CONFIG['coin_api_key']
