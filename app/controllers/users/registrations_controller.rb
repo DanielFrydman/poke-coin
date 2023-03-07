96814b3407748b900dcd0b36e3ea86168c910cab
Daniel Frydman <danielfrydman@id.uff.br>
2023-03-07 12:02:54 -0300

    layout changed to edit and update method

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,20 @@
-
 class Users::RegistrationsController < Devise::RegistrationsController
+  layout :resolve_layout
+
   def create
     super do |resource|
       Wallet.create!(user: resource) if resource.id.present?
+    end
+  end
+
+  private
+
+  def resolve_layout
+    case action_name
+    when 'edit', 'update'
+      'application'
+    else
+      'devise'
     end
   end
 end
